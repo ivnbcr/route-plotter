915abf8ccd7ef43463e03d3116c207189f0d5571
ivnbcr <ivanbacor@gmail.com>
2025-08-02 16:32:01 +0800

    initial setup

---
+++ backend/routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/welcome', function () {
+    return response()->json([
+        'message' => 'Welcome to the API!',
+    ]);
+});
