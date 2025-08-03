509b543e8dfe5f45ff5bd411c2816818c53f40ad
ivnbcr <ivanbacor@gmaill.com>
2025-08-03 09:52:35 +0800

    BE: Added login and register functionality

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/welcome', function () {
     return response()->json([
         'message' => 'Welcome to the API!',
     ]);
 });
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', RegisterController::class);
